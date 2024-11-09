78e41f20fdef16b69321e35f10ffe7102f8d6099
asrithasri <asrithk133@gmail.com>
2024-11-09 11:06:16 +0530

    2nd commit

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -11,6 +11,14 @@ const Users = () => {
         .catch(err => console.log(err))
 
     },[])
+
+    const handleDelete = (id) =>{
+        axios.delete('http://localhost:3001/deleteUser/'+id)
+        .then(res => {console.log(res)
+            window.location.reload()})
+        .catch(err => console.log(err));
+
+    }
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
@@ -33,7 +41,8 @@ const Users = () => {
                             <td>{user.age}</td>
                             <td>
                                 <Link to={"/update/${user._id"} className="btn btn-success">Update</Link>
-                                <button className="btn btn-danger">Delete</button>
+                                <button className="btn btn-danger"
+                                onClick={(e) => handleDelete(user._id)}>Delete</button>
                             </td>
                         </tr>
                     })
